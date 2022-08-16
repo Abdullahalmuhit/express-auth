80b7c77e1e488e1af40a6d660aa0c43f2b6de26f
muhid <muhid@ihealthscreen.org>
2022-08-16 21:04:07 +0600

    Added file

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+const cookieSession = require("cookie-session");
+const db = require("./app/models");
+
+const Role = db.role;
+const app = express();
+app.use(cors());
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    name: "bezkoder-session",
+    secret: "COOKIE_SECRET", // should use as secret environment variable
+    httpOnly: true
+  })
+);
+
+db.sequelize.sync({force: true}).then(() => {
+    console.log('Drop and Resync Db');
+    initial();
+  });
+  function initial() {
+    Role.create({
+      id: 1,
+      name: "user"
+    });
+   
+    Role.create({
+      id: 2,
+      name: "moderator"
+    });
+   
+    Role.create({
+      id: 3,
+      name: "admin"
+    });
+  }
+  
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
